2bb20f0e882074d0564ce2df674b8ec56ea268f5
Leyder Herrera Bello <leyderhr5@gmail.com>
2026-01-11 15:42:46 -0500

    Actualizado Menu Principal
    Agregadas las funciones de los botones Ajustes y Créditos del menú principal.

--- Assets/Scripts/menu_manager.cs
+++ Assets/Scripts/menu_manager.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class menu_manager : MonoBehaviour
 {
+    [SerializeField] private GameObject panelCreditos;
+    [SerializeField] private GameObject panelAjustes;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +29,24 @@ public class menu_manager : MonoBehaviour
     {
         Debug.Log("Saliendo del juego...");
         Application.Quit();
+    }
+
+
+    public void ShowCredits() { 
+        
+        panelCreditos.SetActive(true); 
+    } 
+    
+    public void HideCredits() { 
+        
+        panelCreditos.SetActive(false); 
+    }
+
+    public void ShowAjustes() { 
+        panelAjustes.SetActive(true); 
+    } 
+    
+    public void HideAjustes() { 
+        panelAjustes.SetActive(false); 
     }
 }
