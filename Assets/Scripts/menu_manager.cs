9bef29edf5c7fd5e4244eb4ec66016c2524a7b39
Leyder Herrera Bello <leyderhr5@gmail.com>
2026-01-11 16:50:48 -0500

    Mejoradas funcionalidades del menu principal

--- Assets/Scripts/menu_manager.cs
+++ Assets/Scripts/menu_manager.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class menu_manager : MonoBehaviour
 {
     [SerializeField] private GameObject panelCreditos;
     [SerializeField] private GameObject panelAjustes;
-    
+
+    [SerializeField] private Toggle toggleMusica;
+    [SerializeField] private Image toggleMusicaBackground;
+
+    [SerializeField] private Sprite spriteAudioOn;
+    [SerializeField] private Sprite spriteAudioOff;
+
+    public AudioSource musicaFondo;
+    public AudioSource efectosAudio;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void BotonStart()
@@ -32,21 +42,34 @@ public class menu_manager : MonoBehaviour
     }
 
 
-    public void ShowCredits() { 
-        
-        panelCreditos.SetActive(true); 
-    } 
-    
-    public void HideCredits() { 
-        
-        panelCreditos.SetActive(false); 
+    public void ShowCredits()
+    {
+
+        panelCreditos.SetActive(true);
     }
 
-    public void ShowAjustes() { 
-        panelAjustes.SetActive(true); 
-    } 
-    
-    public void HideAjustes() { 
-        panelAjustes.SetActive(false); 
+    public void HideCredits()
+    {
+
+        panelCreditos.SetActive(false);
+    }
+
+    public void ShowAjustes()
+    {
+        panelAjustes.SetActive(true);
+    }
+
+    public void HideAjustes()
+    {
+        panelAjustes.SetActive(false);
+    }
+
+    public void ToggleMusica(bool activo)
+    {
+        // Activar o desactivar la música 
+        musicaFondo.mute = !activo;
+
+        // Cambiar el icono del toggle 
+        toggleMusicaBackground.sprite = activo ? spriteAudioOn : spriteAudioOff;
     }
 }
